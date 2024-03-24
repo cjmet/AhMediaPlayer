460508fcb0d9d41121cbafd84a250da3c4d40192
CJ Metcalfe <khaaiev@gmail.com>
2024-03-24 19:14:58 -0400

    Second Attempt to move the project into a new clean framework

--- MauiMediaPlayer/publish.bat
+++
@@ -1,1 +0,0 @@
-dotnet publish -f net8.0-windows10.0.19041.0 -c Release -p:RuntimeIdentifierOverride=win10-x64  -p:WindowsPackageType=None -p:WindowsAppSDKSelfContained=true  --output ..\..\Packages\tmp
